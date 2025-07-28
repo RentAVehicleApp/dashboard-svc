47d354cf813922304e4d4c0b2027bcaee80f4357
aglaia <aglaia.hub@gmail.com>
2025-07-28 14:39:51 +0300

    add keep Alive service

---
+++ dashboard-service-api/src/main/java/rent/vehicle/dashboardserviceapi/common/controller/TestController.java
@@ -0,0 +1,22 @@
+package rent.vehicle.dashboardserviceapi.common.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rent.vehicle.dashboardserviceapi.common.service.KeepAliveService;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+    private final KeepAliveService keepAliveService;
+
+    @GetMapping()
+    public String testService() {
+        StringBuilder answer = new StringBuilder();
+        answer.append("log1: ").append(keepAliveService.callCustomerService());
+        answer.append("log2: ").append(keepAliveService.callWorkerService());
+        answer.append("log3: ").append(keepAliveService.callDeviceService());
+
+        return answer.toString();
+    }
+}
